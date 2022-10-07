f20438b36b81f521a3bbb02379e149420b7c437d
YannickBihege <yannickbtheone@hotmail.com>
2022-10-07 21:50:06 +0200

    Clean comment

--- app.py
+++ app.py
@@ -12,28 +12,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///paws.db'
 db = SQLAlchemy(app)
 
 
-"""Information regarding the Pets in the System."""
-'''
-Pets = [
-    {"id": 1, "name": "Nelly", "age": "5 weeks",
-        "bio": "I am a tiny kitten rescued by the good people at Paws Rescue Center. I love squeaky toys and cuddles."},
-    {"id": 2, "name": "Yuki", "age": "8 months",
-     "bio": "I am a handsome gentle-cat. I like to dress up in bow ties."},
-    {"id": 3, "name": "Basker", "age": "1 year",
-     "bio": "I love barking. But, I love my friends more."},
-    {"id": 4, "name": "Mr. Furrkins", "age": "5 years", "bio": "Probably napping."},
-]
-'''
-
-
-"""Information regarding the Users in the System."""
-'''
-Users = [
-    {"id": 1, "full_name": "Pet Rescue Team",
-        "email": "team@pawsrescue.co", "password": "adminpass"},
-]
-'''
-
 with app.app_context():
 
     """Model for Pets."""
